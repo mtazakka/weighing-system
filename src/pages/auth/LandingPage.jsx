8c42f9a9ee13b1158357e51011c84a0042c4235b
mtazakka <extazakka@gmail.com>
2023-09-17 13:02:35 +0700

    initial landing system

--- src/pages/auth/LandingPage.jsx
+++ src/pages/auth/LandingPage.jsx
@@ -1,7 +1,23 @@
-const LandingPage = () => {
-  return (
-    <div>LandingPage</div>
-  )
-}
+import { Center, Container, Title } from '@mantine/core';
 
-export default LandingPage
+export default function LandingPage() {
+  return (
+    <Center
+      sx={{
+        minHeight: '100vh',
+      }}
+    >
+      <Container size="xs" sx={{ width: 480, paddingBottom: 16 }}>
+        <Title
+          align="center"
+          sx={{
+            fontSize: 42,
+            fontWeight: 900,
+          }}
+        >
+          WEIGHING SYSTEM
+        </Title>
+      </Container>
+    </Center>
+  );
+}
