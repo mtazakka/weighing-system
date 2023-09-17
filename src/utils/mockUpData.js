f499fcc1b1bfb3d8449d68522ea362f3436b49ea
mtazakka <extazakka@gmail.com>
2023-09-17 08:40:51 +0700

    [ADD] mockup data

---
+++ src/utils/mockUpData.js
@@ -0,0 +1,42 @@
+export const formMaterialData = {
+    data: [
+      {
+        codeMaterial: 'M001',
+        namaMaterial: 'KAIN',
+        bj: '1',
+        satuan: 'KG',
+      },
+      {
+        codeMaterial: 'M002',
+        namaMaterial: 'SOLAR',
+        bj: '0.89',
+        satuan: 'LT',
+      },
+      {
+        codeMaterial: 'M003',
+        namaMaterial: 'RESIDU',
+        bj: '0.9',
+        satuan: 'LT',
+      },
+    ]
+  };
+
+  export const formLanggananData = {
+    data: [
+      {
+        codeCostumer: 'M001',
+        namaCostumer: 'KAIN',
+        alamat: '1',
+      },
+      {
+        codeCostumer: 'M002',
+        namaCostumer: 'SOLAR',
+        alamat: '0.89',
+      },
+      {
+        codeCostumer: 'M003',
+        namaCostumer: 'RESIDU',
+        alamat: '0.9',
+      },
+    ]
+  };
